c7aacf90e426acd57ce41e4fb2fdc8dd369ded36
maiconst001 <110488467+maiconst001@users.noreply.github.com>
2023-05-20 03:08:57 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,16 @@ setInterval(() => {
     var frame = document.querySelector('iframe')
 frame.style.height = (0.57 * Number(frame.getClientRects()[0].width)) + 'px';
 }, 500);
+
+
+
+
+
+
+
+
+
+
+
+
+

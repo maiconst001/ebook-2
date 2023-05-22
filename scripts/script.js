c94a1efc035ab399d0d0c72313204aa37b14315d
maiconst001 <110488467+maiconst001@users.noreply.github.com>
2023-05-21 23:27:06 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,19 @@
+var slide1 = document.querySelector('.slide-s')
+var slide2 = document.querySelector('.slide-s2')
+
+
 setInterval(() => {
     var frame = document.querySelector('iframe')
-frame.style.height = (0.57 * Number(frame.getClientRects()[0].width)) + 'px';
+    frame.style.height = (0.57 * Number(frame.getClientRects()[0].width)) + 'px';
+
+
+    if (window.innerWidth <= 1000) {
+        slide2.style.display = 'none'   
+        slide1.style.display = 'block'   
+    }else {
+        slide2.style.display = 'block'   
+        slide1.style.display = 'none'   
+    }
 }, 500);
 
 
